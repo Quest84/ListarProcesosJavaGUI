41aa0492ade759c55a824a3c8a76497c10a72d85
Quest84 <QuestionSleep123@gmail.com>
2020-10-27 22:40:35 -0600

    Agregada Columna EstadoProceso

--- src/Paquetaxo/Operaciones.java
+++ src/Paquetaxo/Operaciones.java
@@ -7,8 +7,10 @@ package Paquetaxo;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -16,45 +18,88 @@ import javax.swing.table.DefaultTableModel;
  * @author Manuel
  */
 public class Operaciones {
+
     private DefaultTableModel DefaultTM;
-    
-    
-    public Operaciones(){
-        
+
+    public Operaciones() {
+
     }
-    
-    private DefaultTableModel setColumnas(){
+
+    private DefaultTableModel setColumnas() {
         DefaultTM = new DefaultTableModel();
         DefaultTM.addColumn("Nombre de la Imagen");
         DefaultTM.addColumn("PID");
-        DefaultTM.addColumn("Nombre de Sesion");
+        DefaultTM.addColumn("Nombre de Sesión");
         DefaultTM.addColumn("Numero de Sesión");
         DefaultTM.addColumn("Uso de Memoria");
-        
+        DefaultTM.addColumn("Estado del Proceso");
+
         return DefaultTM;
     }
-    
-    public DefaultTableModel getDatos(){
-        try{
+
+    int makeRandom() {
+        int randomInt = 0;
+        for (int i = 0; i < 3; i++) {
+            randomInt = (int) (5.0 * Math.random());
+            System.out.println("pseudo random int between 1 and 10 : " + randomInt);
+        }
+        return randomInt;
+    }
+
+    public DefaultTableModel getDatos() {
+        try {
             setColumnas();
-            
+
             String line;
-            Process p = Runtime.getRuntime().exec
-                    (System.getenv("windir") + "\\system32\\" + "tasklist.exe /fo csv /nh");
+            Process p = Runtime.getRuntime().exec(System.getenv("windir") + "\\system32\\" + "tasklist.exe /fo csv /nh");
             // Se pueden usar los parámetros en tasklist  /fo csv /nh para sacar la info en formato CSV
-            BufferedReader input =
-                    new BufferedReader (new InputStreamReader(p.getInputStream()));
+            BufferedReader input
+                    = new BufferedReader(new InputStreamReader(p.getInputStream()));
             while ((line = input.readLine()) != null) {
-                String[] dataLine = line.split(",");
+                int x = makeRandom();
+                line = line + "," + x;
+                line = line.replace("\",", ";");
+                String[] dataLine = line.split(";");
                 DefaultTM.addRow(dataLine);
             }
+
             input.close();
-            
-        }catch (Exception e){
+
+        } catch (Exception e) {
             e.printStackTrace();
+        }
+
+        return DefaultTM;
+    }
+
+    /*public DefaultTableModel getDatos(int estado) {
+        switch (estado) {
+            case 0:
+                try {
+                    setColumnas();
+
+                    String line;
+                    Process p = Runtime.getRuntime().exec(System.getenv("windir") + "\\system32\\" + "tasklist.exe /fo csv /nh");
+                    // Se pueden usar los parámetros en tasklist  /fo csv /nh para sacar la info en formato CSV
+                    BufferedReader input
+                            = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                    while ((line = input.readLine()) != null) {
+                        String[] dataLine = line.split(",");
+                        DefaultTM.addRow(dataLine);
+                    }
+                    input.close();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                break;
+
+            case 1:
+                
+                break;
         }
         
         return DefaultTM;
-    }
-        
+    }*/
 }
